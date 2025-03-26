d0af6237e9bbb48fe9ca3cb5a3db848aab55238e
Klon <www.vfdrey@gmail.com>
2025-03-26 20:17:41 +0400

    lab_3 init

--- module_3/lab_2/digital-department-application/src/app/app.routes.ts
+++ module_3/lab_2/digital-department-application/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GreetingComponent } from './greeting/greeting.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component: GreetingComponent},
+    {path:"page1", component: Page1Component},
+    {path:"page2", component: Page2Component}
+];
