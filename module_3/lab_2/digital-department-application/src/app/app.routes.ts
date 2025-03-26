1d6b6959e0d002a9ae3915a266e7f3091e0d9601
Klon <www.vfdrey@gmail.com>
2025-03-26 22:22:37 +0400

    done task 5

--- module_3/lab_2/digital-department-application/src/app/app.routes.ts
+++ module_3/lab_2/digital-department-application/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { GreetingComponent } from './greeting/greeting.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
+import { confirmInGuard } from './core/guards/confirm-in.guard';
+import { confirmOutGuard } from './core/guards/confirm-out.guard';
 
 export const routes: Routes = [
     {path:"", component: GreetingComponent},
-    {path:"page1/:id", component: Page1Component},
-    {path:"page2", component: Page2Component},
+    {path:"page1/:id", component: Page1Component, canActivate: [confirmInGuard]},
+    {path:"page2", component: Page2Component, canDeactivate: [confirmOutGuard]},
     {path:"page3", component: Page3Component}
 ];
