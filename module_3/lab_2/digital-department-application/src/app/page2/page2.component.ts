d50827dbad404f5d10d64182cce001453dc72d48
Klon <www.vfdrey@gmail.com>
2025-03-26 21:20:25 +0400

    done task4

--- module_3/lab_2/digital-department-application/src/app/page2/page2.component.ts
+++ module_3/lab_2/digital-department-application/src/app/page2/page2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page2',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './page2.component.css'
 })
 export class Page2Component {
+  constructor(private router: Router) {
+
+  }
+
+  goMain(): void {
+    this.router.navigateByUrl("/")
+  }
 
 }
